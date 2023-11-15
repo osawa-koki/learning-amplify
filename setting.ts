9e707af48c6345634b03fa1067a8084d66627abd
osawa-koki <koki@osawa.dev>
2023-11-15 18:16:03 +0900

    クライアントプロジェクトの初期化。

---
+++ setting.ts
@@ -0,0 +1,11 @@
+import Env from './next.config.js'
+const isProd = process.env.NODE_ENV === 'production'
+
+const setting = {
+  isProd,
+  basePath: Env.basePath,
+  apiPath: isProd ? '' : 'http://localhost:8000',
+  title: '🌭 Learning Amplify 🌭'
+}
+
+export default setting
